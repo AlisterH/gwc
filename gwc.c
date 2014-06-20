3bda3b8f3d4d87151254fd455b9271646de63dad
Alister Hood <alister.hood@gmail.com>
2014-06-21 10:35:36 +1200

    add license information to the about window

--- gwc.c
+++ gwc.c
@@ -1816,11 +1816,24 @@ void destroy(GtkWidget * widget, gpointer data)
 void about(GtkWidget *window)
 {
     const gchar *authors[] = { "Jeffrey J. Welty", "James Tappin", "Ian Leonard", "Bill Jetzer", "Charles Morgon", "Frank Freudenberg", "Thiemo Gehrke", "Rob Frohne", NULL };
+    const gchar* LICENSE="This program is free software; you can redistribute it and/or "
+			 "modify it under the terms of the GNU General Public License "
+			 "as published by the Free Software Foundation; either version 2 "
+			 "of the License, or (at your option) any later version.\n\n"
+			 "This program is distributed in the hope that it will be useful, "
+			 "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+			 "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+			 "GNU General Public License for more details.\n\n"
+			 "You should have received a copy of the GNU General Public License "
+			 "along with this program; if not, write to the Free Software "
+			 "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.";
 
     gtk_show_about_dialog(GTK_WINDOW( window ),
 				    "program-name", "Gnome Wave Cleaner",
 				    "version", VERSION,
 				    "copyright", "Copyright 2001, 2002, 2003, 2004, 2005 Redhawk.org",
+				    "license", LICENSE,
+				    "wrap-license", TRUE,
 				    "website", "http://gwc.sourceforge.net/",
 				    "comments", "An application to aid in denoising (hiss & clicks) of audio files",
 				    "authors", authors,
