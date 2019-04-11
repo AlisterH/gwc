a7745acd389badca8706d4cea7ea9eba3c199620
Alister Hood <alister.hood@gmail.com>
2019-04-12 02:09:11 +1200

    bump the build version

--- gwc.h
+++ gwc.h
@@ -49,7 +49,7 @@
 
 #define GWC_VERSION_MAJOR 0
 #define GWC_VERSION_MINOR 22
-#define VERSION "0.22"
+#define VERSION "0.22-02"
 #define GWC_POINT_HANDLE 0x01
 #define SBW  128	/* Sample Block Width, the number of audio samples summarized in one block  */
 #define PROGRESS_UPDATE_INTERVAL 0.5	/* update status bar every 1/2  second on long edit operations */
